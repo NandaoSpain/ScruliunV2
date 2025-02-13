9ace2f940d4b0b591ce36b03237085f95cd9c054
Fernando Rodrigues <117524258+NandaoSpain@users.noreply.github.com>
2025-02-13 16:44:51 -0300

    update

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,7 +4,6 @@ import { sessionsRoutes } from "./sessions-routes";
 import { teamsRoutes } from "./teams-routes";
 import { tasksRoutes } from "./tasks-routes";
 import { teamMembersRoutes } from "./team-members-routes";
-import { taskHistoryRoutes } from "./task-history-routes";
 
 const routes = Router()
 
@@ -13,6 +12,5 @@ routes.use("/sessions", sessionsRoutes)
 routes.use("/teams", teamsRoutes)
 routes.use("/tasks", tasksRoutes)
 routes.use("/team-members", teamMembersRoutes)
-routes.use("/task-history", taskHistoryRoutes)
 
 export { routes }
